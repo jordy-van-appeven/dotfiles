762b5bba8d5e22b255478501cc14398810b76b68
Jordy van Appeven <jordyvanappeven@gmail.com>
2023-06-18 22:48:36 +0200

    [qtile] Swap 'esc' and 'caps' keys

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -4,4 +4,6 @@ nitrogen --restore &
 picom --experimental-backends -b &
 # Disable touchpad while typing
 syndaemon -i 0.5 -t -k &
+# Swap escape with caps-lock
+setxkbmap -option caps:swapescape &
 
