c773d04c8fca3a6232fe45e05fb609094fbf431a
Jordy van Appeven <jordyvanappeven@gmail.com>
2023-12-22 17:14:12 +0100

    [nvim] Add 'bufferline' plugin

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,24 @@
+return {
+    "akinsho/bufferline.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    version = "*",
+    -- https://github.com/catppuccin/nvim#special-integrations:~:text=bufferline.nvim-,Special,-coc.nvim
+    after = "catppuccin",
+    config = function()
+        require("bufferline").setup {
+
+            highlights = require("catppuccin.groups.integrations.bufferline").get(),
+
+            options = {
+                mode = "tabs",
+                separator_style = "slant",
+                offsets = {
+                    {
+                        filetype = "NvimTree",
+                        text = "NvimTree",
+                    }
+                },
+            },
+        }
+    end
+}
