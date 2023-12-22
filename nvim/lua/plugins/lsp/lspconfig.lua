8d7dee1426d6968cde59de4655f719a4a366c72c
Jordy van Appeven <jordyvanappeven@gmail.com>
2023-12-22 23:35:10 +0100

    [nvim] Add diagnostic symbols

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -62,6 +62,13 @@ return {
             vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
         end
 
+        -- Change the Diagnostic symbols in the sign column (gutter)
+        local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+        end
+
         -- used to enable autocompletion (assign to every lsp server config)
         local capabilities = cmp_nvim_lsp.default_capabilities()
 
