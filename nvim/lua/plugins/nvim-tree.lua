6386ece638f02bd17dadd1e039b1c35fd2333e75
Jordy van Appeven <jordyvanappeven@gmail.com>
2023-12-22 14:34:00 +0100

    [nvim] Add 'nvim-tree' plugin

---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,16 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+        require("nvim-tree").setup({
+            actions = {
+                open_file = {
+                    quit_on_open = true,
+                },
+            },
+        })
+        vim.keymap.set("n", "<M-1>", "<cmd>NvimTreeFindFileToggle<CR>")
+    end
+}
