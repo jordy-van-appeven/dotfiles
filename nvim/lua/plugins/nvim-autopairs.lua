03ed49154a3a74628e0c1e904f1c1945606291cb
Jordy van Appeven <jordyvanappeven@gmail.com>
2023-12-22 16:53:21 +0100

    [nvim] Add 'nvim-autopairs' plugin

---
+++ nvim/lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,21 @@
+return {
+    "windwp/nvim-autopairs",
+    event = { "InsertEnter" },
+    dependencies = {
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        local autopairs = require("nvim-autopairs")
+
+        autopairs.setup({
+            -- enable treesitter
+            check_ts = true,
+        })
+
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        local cmp = require("cmp")
+
+        -- make autopairs and completion work together
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+}
