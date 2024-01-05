fc4c19d84a554461346c56f44098cbae7975c7c7
Jordy van Appeven <jordyvanappeven@gmail.com>
2024-01-05 17:58:54 +0100

    [nvim] Lua helper function 'Print' added

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 require("core.options")
 require("core.keymaps")
+require("core.utils")
 require(".lazy")
 
---
+++ nvim/lua/core/utils.lua
@@ -0,0 +1,5 @@
+-- Helper function to print lua tables
+Print = function(v)
+    print(vim.inspect(v))
+    return v
+end
