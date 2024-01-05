3b6b878cd195054f4bb90af85930951c7f962870
Jordy van Appeven <jordyvanappeven@gmail.com>
2024-01-05 17:21:16 +0100

    [nvim] Plugin 'nvim-dap-ui' added

---
+++ nvim/lua/plugins/dap/nvim-dap-ui.lua
@@ -0,0 +1,26 @@
+return {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap" },
+    config = function()
+        local dap = require("dap")
+
+        local dapui = require("dapui")
+        dapui.setup({})
+
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
+    end
+}
