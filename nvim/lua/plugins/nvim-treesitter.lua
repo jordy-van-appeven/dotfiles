46342762acf1900b86ee0a9b586efe9e67cb37a5
Jordy v. Appeven <jordyvanappeven@gmail.com>
2024-02-04 17:51:29 +0100

    [nvim] Treesitter add additional parsers

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,26 +1,47 @@
+local nvim_treesitter_config = function()
+    local treesitter = require("nvim-treesitter.configs")
+
+    treesitter.setup({
+        ensure_installed = {
+            "bash",
+            "c",
+            "cmake",
+            "cpp",
+            "csv",
+            "cuda",
+            "dockerfile",
+            "json5",
+            "jsonc",
+            "latex",
+            "lua",
+            "markdown",
+            "python",
+            "query",
+            "sql",
+            "vim",
+            "vimdoc",
+            "yaml",
+        },
+
+        highlight = {
+            enable = true,
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<C-space>", -- set to `false` to disable one of the mappings
+                node_incremental = "<C-space>",
+                scope_incremental = false,
+                node_decremental = "<bs>",
+            },
+        },
+    })
+end
+
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
     event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local treesitter = require("nvim-treesitter.configs")
-
-        treesitter.setup({
-            ensure_installed = { "c", "jsonc", "lua", "python", "query", "vim", "vimdoc" },
-
-            highlight = {
-                enable = true,
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-space>", -- set to `false` to disable one of the mappings
-                    node_incremental = "<C-space>",
-                    scope_incremental = false,
-                    node_decremental = "<bs>",
-                },
-            },
-        })
-    end
+    config = nvim_treesitter_config,
 }
