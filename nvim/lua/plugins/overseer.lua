c9aa1d9318bf648accd0ebad579f4acc8fd774bd
Jordy v. Appeven <jordyvanappeven@gmail.com>
2024-02-04 16:21:41 +0100

    [nvim] Overseer sends vscode task output to quickfix

--- nvim/lua/plugins/overseer.lua
+++ nvim/lua/plugins/overseer.lua
@@ -10,6 +10,31 @@ return {
             },
             -- templates = { "builtin", "user.cpp_build", "user.python_script" },
             templates = { "builtin" },
+
+            task_list = {
+                default_detail = 1,
+                -- min_width = {40, 0.99},
+                min_height = {8, 25},
+                direction = "bottom"
+            },
+
+            component_aliases = {
+                -- Most tasks are initialized with the default components
+                default = {
+                    { "display_duration", detail_level = 1 },
+                    "on_exit_set_status",
+                    "on_complete_notify",
+                    "on_complete_dispose",
+                },
+                -- Tasks from tasks.json use these components
+                default_vscode = {
+                    {"on_output_quickfix",
+                    items_only = true,
+                    open_on_match = false,
+                    tail = true},
+                    "default",
+                },
+            },
         })
 
         -- json decoder with support for JSON5 https://github.com/stevearc/overseer.nvim/blob/master/doc/third_party.md#dap
