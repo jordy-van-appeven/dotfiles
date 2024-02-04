50308612fe5e73a6600b2ee040efda4a6c8e4b7e
Jordy v. Appeven <jordyvanappeven@gmail.com>
2024-02-04 14:20:07 +0100

    [nvim] Command keymapping without changing modes

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,17 +1,17 @@
 -- general keymapping
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<ESC>", ":nohl<CR>")
+vim.keymap.set("n", "<ESC>", "<cmd>nohl<CR>")
 
-vim.keymap.set("n", "<C-H>", ":vertical resize +3<CR>")
-vim.keymap.set("n", "<C-L>", ":vertical resize -3<CR>")
-vim.keymap.set("n", "<C-J>", ":resize +3<CR>")
-vim.keymap.set("n", "<C-K>", ":resize -3<CR>")
+vim.keymap.set("n", "<C-H>", "<cmd>vertical resize +3<CR>")
+vim.keymap.set("n", "<C-L>", "<cmd>vertical resize -3<CR>")
+vim.keymap.set("n", "<C-J>", "<cmd>resize +3<CR>")
+vim.keymap.set("n", "<C-K>", "<cmd>resize -3<CR>")
 
-vim.keymap.set("n", "<leader>to", ":tabnew<CR>", { desc = "Open new tab" })
-vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", { desc = "Close current tab" })
-vim.keymap.set("n", "<leader>tn", ":tabn<CR>", { desc = "Go to next tab" })
-vim.keymap.set("n", "<leader>tp", ":tabp<CR>", { desc = "Go to previous tab" })
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 vim.keymap.set("n", "<leader>tf", "<C-w>T", { desc = "Move current buffer to new tab" })
 
 vim.keymap.set("c", "<M-BS>", "<C-w>", { desc = "Delete 'word'" })
