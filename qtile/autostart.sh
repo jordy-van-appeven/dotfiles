80dec4a5d57cefaee65d5929b13309d595a1c703
Jordy v. Appeven <jordyvanappeven@gmail.com>
2024-02-04 18:01:36 +0100

    [qtile] Config to disable touchpad while typing updated

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -3,7 +3,7 @@
 nitrogen --restore &
 picom --experimental-backends -b &
 # Disable touchpad while typing
-syndaemon -i 0.5 -t -k &
+syndaemon -i 3 -d -t -k &
 # Keyboard layout and remapping
 kmonad ~/.config/kmonad/laptop.kbd &
 
