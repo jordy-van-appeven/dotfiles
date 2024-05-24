518f6c06ea5fb7f7f6e0e0fc8a2e50863ede323f
Jordy v. Appeven <jordyvanappeven@gmail.com>
2024-05-24 19:40:13 +0200

    [nvim] Trouble keymap updated

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,19 +1,27 @@
-local trouble_config = function()
-    -- vim.keymap.set("n", "<leader>D", function() require("trouble").toggle() end)
-    vim.keymap.set("n", "<leader>dD", function() require("trouble").toggle("workspace_diagnostics") end)
-    vim.keymap.set("n", "<leader>dd", function() require("trouble").toggle("document_diagnostics") end)
-    vim.keymap.set("n", "<leader>dq", function() require("trouble").toggle("quickfix") end)
-    vim.keymap.set("n", "<leader>dl", function() require("trouble").toggle("loclist") end)
-    vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+local config_function = function()
+	-- vim.keymap.set("n", "<leader>D", function() require("trouble").toggle() end)
+	require("trouble").setup({ height = 20 })
+	vim.keymap.set("n", "<leader>dD", function()
+		require("trouble").toggle("workspace_diagnostics")
+	end)
+	vim.keymap.set("n", "<leader>dd", function()
+		require("trouble").toggle("document_diagnostics")
+	end)
+	vim.keymap.set("n", "<leader>dq", function()
+		vim.cmd("cclose")
+		require("trouble").toggle("quickfix")
+	end)
+	vim.keymap.set("n", "<leader>dl", function()
+		vim.cmd("lclose")
+		require("trouble").toggle("loclist")
+	end)
+	vim.keymap.set("n", "gR", function()
+		require("trouble").toggle("lsp_references")
+	end)
 end
 
 return {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = trouble_config,
-    opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-    },
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = config_function,
 }
