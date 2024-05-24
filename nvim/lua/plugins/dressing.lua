ff748457593888b4e4ff29e99e254b473864eb58
Jordy v. Appeven <jordyvanappeven@gmail.com>
2024-05-24 19:41:13 +0200

    [nvim] Dressing configuration updated

--- nvim/lua/plugins/dressing.lua
+++ nvim/lua/plugins/dressing.lua
@@ -1,4 +1,19 @@
 return {
-    "stevearc/dressing.nvim",
-    event = "VeryLazy",
+	"stevearc/dressing.nvim",
+	event = "VeryLazy",
+	config = function()
+		require("dressing").setup({
+			input = {
+				win_options = {
+					-- Disable line wrapping
+					wrap = false,
+					-- Indicator for when text exceeds window
+					list = true,
+					listchars = "precedes:…,extends:…",
+					-- Increase this for more context when text scrolls off the window
+					sidescrolloff = 4,
+				},
+			},
+		})
+	end,
 }
