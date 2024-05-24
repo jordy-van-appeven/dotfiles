e8c937bc3d66244d11e823c0cf4d2b4dcba9184c
Jordy v. Appeven <jordyvanappeven@gmail.com>
2024-05-24 19:30:14 +0200

    [nvim] Bufferline shown with more than two tabs

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,23 +1,26 @@
+local config_function = function()
+    require("bufferline").setup {
+        -- https://github.com/catppuccin/nvim#special-integrations:~:text=bufferline.nvim-,Special,-coc.nvim
+        highlights = require("catppuccin.groups.integrations.bufferline").get(),
+        options = {
+            always_show_bufferline = false,     -- Setting 'showtabline'
+            mode = "tabs",
+            numbers = "none",
+            separator_style = "slant",
+            offsets = {
+                {
+                    filetype = "NvimTree",
+                    text = "",
+                }
+            },
+        },
+    }
+end
+
 return {
     "akinsho/bufferline.nvim",
     dependencies = { "catppuccin/nvim", "nvim-tree/nvim-web-devicons" },
     version = "*",
-    enabled = false,
-    config = function()
-        require("bufferline").setup {
-            -- https://github.com/catppuccin/nvim#special-integrations:~:text=bufferline.nvim-,Special,-coc.nvim
-            highlights = require("catppuccin.groups.integrations.bufferline").get(),
-
-            options = {
-                mode = "tabs",
-                separator_style = "slant",
-                offsets = {
-                    {
-                        filetype = "NvimTree",
-                        text = "NvimTree",
-                    }
-                },
-            },
-        }
-    end
+    enabled = true,
+    config = config_function,
 }
