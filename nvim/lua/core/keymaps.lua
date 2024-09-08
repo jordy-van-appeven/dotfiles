6477385fd27a8ba17bc7baa409754cb60ed149b2
Jordy v. Appeven <jordyvanappeven@gmail.com>
2024-09-08 18:11:57 +0200

    [nvim] Buffer duplicated to new tab

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -12,7 +12,7 @@ vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
-vim.keymap.set("n", "<leader>tf", "<C-w>T", { desc = "Move current buffer to new tab" })
+vim.keymap.set("n", "<leader>tf", "<C-w>s<C-w>T", { desc = "Duplicate current buffer to new tab" })
 
 vim.keymap.set("c", "<M-BS>", "<C-w>", { desc = "Delete 'word'" })
 vim.keymap.set("t", "<ESC>", "<C-\\><C-n>", { desc = "Return to normal mode" })
