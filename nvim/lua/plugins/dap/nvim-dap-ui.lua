47cf65b2fd223820e36df35c40384baa3b737f8e
Jordy v. Appeven <jordyvanappeven@gmail.com>
2024-09-08 18:14:57 +0200

    [nvim] Layout of dap-ui plugin is modified

--- nvim/lua/plugins/dap/nvim-dap-ui.lua
+++ nvim/lua/plugins/dap/nvim-dap-ui.lua
@@ -1,52 +1,72 @@
 local nvim_dap_ui_config = function()
-    local dap = require("dap")
+	local dap = require("dap")
 
-    local dapui = require("dapui")
-    dapui.setup({
-        layouts = { {
-            elements = { {
-                id = "scopes", size = 0.25
-            }, {
-                id = "breakpoints", size = 0.25
-            }, {
-                id = "stacks", size = 0.25
-            }, {
-                id = "watches", size = 0.25
-            } },
-            position = "left",
-            size = 35
-        }, {
-            elements = { {
-                id = "repl", size = 1.0
-            }, },
-            position = "bottom",
-            size = 10
-        } },
+	local dapui = require("dapui")
+	dapui.setup({
+		expand_lines = true,
+		force_buffers = true,
+		layouts = {
+			{
+				elements = {
+					{
+						id = "repl",
+						size = 0.60,
+					},
+					{
+						id = "scopes",
+						size = 0.40,
+					},
+					--                {
+					-- 	id = "console",
+					-- 	size = 0.20,
+					-- },
+				},
+				position = "bottom",
+				size = 10,
+			},
+			{
+				elements = {
+					{
+						id = "breakpoints",
+						size = 0.25,
+					},
+					{
+						id = "stacks",
+						size = 0.50,
+					},
+					{
+						id = "watches",
+						size = 0.25,
+					},
+				},
+				position = "right",
+				size = 35,
+			},
+		},
+	})
 
-    })
+	dap.listeners.before.attach.dapui_config = function()
+		dapui.open()
+	end
 
-    dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-    end
+	dap.listeners.before.launch.dapui_config = function()
+		dapui.open()
+	end
 
-    dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-    end
+	dap.listeners.before.event_terminated.dapui_config = function()
+		-- dapui.close()
+	end
 
-    dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-    end
+	dap.listeners.before.event_exited.dapui_config = function()
+		dapui.close()
+	end
 
-    dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-    end
-
-    -- Keymaps
-    vim.keymap.set("n", "<leader>dt", dapui.toggle)
+	-- Keymaps
+	vim.keymap.set("n", "<leader>dt", dapui.toggle)
 end
 
 return {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-    config = nvim_dap_ui_config,
+	"rcarriga/nvim-dap-ui",
+	dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+	config = nvim_dap_ui_config,
 }
