62534f484a513def0e23669c7e65645d7a0ffe7b
Jordy v. Appeven <jordyvanappeven@gmail.com>
2024-09-24 22:20:16 +0200

    [nvim] Syntax highlighting in nvim-dap REPL

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,47 +1,52 @@
 local nvim_treesitter_config = function()
-    local treesitter = require("nvim-treesitter.configs")
+	require("nvim-dap-repl-highlights").setup()
 
-    treesitter.setup({
-        ensure_installed = {
-            "bash",
-            "c",
-            "cmake",
-            "cpp",
-            "csv",
-            "cuda",
-            "dockerfile",
-            "json5",
-            "jsonc",
-            "latex",
-            "lua",
-            "markdown",
-            "python",
-            "query",
-            "sql",
-            "vim",
-            "vimdoc",
-            "yaml",
-        },
+	local treesitter = require("nvim-treesitter.configs")
+	treesitter.setup({
+		ensure_installed = {
+			"bash",
+			"c",
+			"cmake",
+			"cpp",
+			"csv",
+			"cuda",
+			"dap_repl",
+			"dockerfile",
+			"json5",
+			"jsonc",
+			"latex",
+			"lua",
+			"markdown",
+			"python",
+			"query",
+			"sql",
+			"vim",
+			"vimdoc",
+			"yaml",
+		},
 
-        highlight = {
-            enable = true,
-        },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<C-space>", -- set to `false` to disable one of the mappings
-                node_incremental = "<C-space>",
-                scope_incremental = false,
-                node_decremental = "<bs>",
-            },
-        },
-    })
+		highlight = {
+			enable = true,
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-space>", -- set to `false` to disable one of the mappings
+				node_incremental = "<C-space>",
+				scope_incremental = false,
+				node_decremental = "<bs>",
+			},
+		},
+	})
 end
 
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
-    event = { "BufReadPre", "BufNewFile" },
-    config = nvim_treesitter_config,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		"LiadOz/nvim-dap-repl-highlights",
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	config = nvim_treesitter_config,
 }
