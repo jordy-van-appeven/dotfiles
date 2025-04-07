ca15923c0434ab29e666deb0d742de12de315c21
Jordy v. Appeven <jordyvanappeven@gmail.com>
2025-04-07 19:01:48 +0200

    [nvim,tmux] Pane navigation now done with 'Alt' key

--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -8,9 +8,9 @@ return {
         vim.g.tmux_navigator_save_on_switch = 1
     end,
     config = function()
-        vim.api.nvim_set_keymap('n', '<C-w>l', ':TmuxNavigateRight<CR>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', '<C-w>j', ':TmuxNavigateDown<CR>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', '<C-w>h', ':TmuxNavigateLeft<CR>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', '<C-w>k', ':TmuxNavigateUp<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<M-l>', ':TmuxNavigateRight<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<M-j>', ':TmuxNavigateDown<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<M-h>', ':TmuxNavigateLeft<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<M-k>', ':TmuxNavigateUp<CR>', { noremap = true, silent = true })
     end
 }
