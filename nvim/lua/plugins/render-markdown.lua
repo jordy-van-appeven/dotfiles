bd87cef07048f6c8abba2bb3d20c07cbbe242956
Jordy v. Appeven <jordyvanappeven@gmail.com>
2025-04-07 18:50:08 +0200

    [nvim] Markdown render plugin added

---
+++ nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,14 @@
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	opts = {
+		heading = {
+			enabled = true,
+			sign = false,
+			icons = { "" },
+		},
+	},
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+}
