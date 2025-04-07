a283626c14dbd9e27b7ff0b1aba7cf379526d4f7
Jordy v. Appeven <jordyvanappeven@gmail.com>
2025-04-07 19:01:28 +0200

    [qtile,tmux] Catppuccin Mocha theme is used

--- qtile/theme.py
+++ qtile/theme.py
@@ -2,19 +2,18 @@
 
 # Theme Color Dictionaries
 
-dracula = {
-        "bar_background": "#282a36",
-        "bar_foreground": "#f8f8f2",
-        "widget_background": "#282a36",
-        "widget_foreground": "#f8f8f2",
+theme = {
+        "bar_background": "#222436",
+        "bar_foreground": "#c8d3f5",
+        "widget_background": "#222436",
+        "widget_foreground": "#c8d3f5",
         "active": "#8d62a9",
         "inactive": "#44475a",
         "comment": "#6272a4",
-        "cyan":	"#8be9fd",
-        "green": "#50fa7b",
-        "orange": "#ffb86c",
-        "pink":	"#ff79c6",
-        "red": "#ff5555",
-        "yellow": "#f1fa8c"
+        "cyan":	"#86e1fc",
+        "green": "#c3e88d",
+        "pink":	"#c099ff",
+        "red": "#ff757f",
+        "yellow": "#ffc777"
         }
 
