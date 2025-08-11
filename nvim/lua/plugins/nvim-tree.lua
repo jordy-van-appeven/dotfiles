70ec93455c4242a3083e26803425f84e71de3c4e
Jordy v. Appeven <jordyvanappeven@gmail.com>
2025-08-11 08:36:56 +0200

    [nvim-tree] Keybind added to open file directory in explorer

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,31 +1,32 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-        require("nvim-tree").setup({
-            actions = {
-                open_file = {
-                    quit_on_open = true,
-                    window_picker = {
-                        enable = false,
-                    }
-                },
-            },
-            filters = {
-                git_ignored = false,
-            },
-            git = {
-                enable = false,
-            },
-            view = {
-                adaptive_size = true,
-                centralize_selection = true,
-                relativenumber = true,
-                signcolumn = "no",
-            },
-        })
-        vim.keymap.set("n", "<M-1>", "<cmd>NvimTreeFindFileToggle<CR>")
-    end
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("nvim-tree").setup({
+			actions = {
+				open_file = {
+					quit_on_open = true,
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+			filters = {
+				git_ignored = false,
+			},
+			git = {
+				enable = false,
+			},
+			view = {
+				adaptive_size = true,
+				centralize_selection = true,
+				relativenumber = true,
+				signcolumn = "no",
+			},
+		})
+		vim.keymap.set("n", "<M-1>", "<cmd>NvimTreeFindFileToggle<CR>")
+        vim.keymap.set("n", "gx", ":silent !xdg-open %:p:h &<CR>", { noremap = true, silent = true })
+	end,
 }
